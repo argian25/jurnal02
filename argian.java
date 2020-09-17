b3e9bcbaa2531e3eb02e58a50a40a9fca7e69ed3
arguxx <40434824+arguxx@users.noreply.github.com>
2020-09-17 21:30:33 +0700

    argian.java

---
+++ argian.java
@@ -0,0 +1,9 @@
+public class argian {
+
+    public static void main(String[] args){
+        System.out.println("Nama : Argian Raditya Putra"); 
+		 System.out.println("NIM: 6706202022"); 
+		 System.out.println("Kelas : D3IF 44-03");
+        
+    }
+}
